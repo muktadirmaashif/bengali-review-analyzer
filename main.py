a6da5273385725fbc0a4505df1958ee29c62dd5b
Muktadir M Aashif <muktadirmaashif@gmail.com>
2021-12-24 13:46:45 +0600

    create invalid url flash

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, flash
 #from kivy.uix.screenmanager import Screen
 import Scraper 
 from Scraper import *
-
+import validators
 
 app = Flask(__name__)
+app.secret_key = 'super secret'
 
 """ @app.route("/")
 def hello_world():
@@ -14,14 +15,23 @@ def hello_world():
 def index():
     if request.method == "POST":
         url = request.form["url"]
-        print(url)
-        main(url)
-        
-        scrap = {'title': Scraper.Bangla_title,
-                    'author': Scraper.Bangla_author,
-                    'pos': mlModel.posPercentage,
-                    'neg': mlModel.negPercentage}
-        return render_template("details.html", scrap= scrap)
+        is_url = validators.url(url)
+        if (is_url):
+            
+            print(url)
+            main(url)
+            
+            scrap = {'title': Scraper.Bangla_title,
+                        'author': Scraper.Bangla_author,
+                        'pos': mlModel.posPercentage,
+                        'neg': mlModel.negPercentage}
+            
+            
+            return render_template("details.html", scrap= scrap)
+        else:
+            flash('Invalid URL! Please enter a valid book URL from rokomari.com.', 'error')
+            return render_template('index.html')
+
     else:
         return render_template('index.html')
 
